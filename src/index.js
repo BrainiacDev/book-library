dbd8dedb78fc70ac02ea2db88fd90321c4cf4eb3
Brainiac Tech <olasunkanmiabdullateef@gmail.com>
2023-05-28 17:42:18 +0100

    booklib

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookDetails from "./components/BookDetails/BookDetails";
+import BookList from './components/BooList/BookList';
+import { AppProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='book' element={<BookList />} />
+          <Route path='/book/:id' element={<BookDetails />} />
+        </Route>
+      </Routes>
+    </Router>
+  </AppProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
